bd03e86d0d3e39efa3d2eb5531f6aa3673d06f7c
delbug7 <delbug7@gmail.com>
2020-06-23 11:14:05 +0200

    Fix du programme d'installation et du login.

--- installation/app/controller/varExists.php
+++ installation/app/controller/varExists.php
@@ -4,11 +4,10 @@ namespace Install;
 defined('_HUBACCES') or header('Location: /index.php');
 
 $message = '';
-if(!file_exists(HUBPATH_TMP)){
-    mkdir(HUBPATH_TMP);
-}
 if(!file_exists(HUBPATH_TMP . '/tmp.php')){
-    Fopen::createFile(HUBPATH_TMP . '/tmp.php');
+    if(!Fopen::createFile(HUBPATH_TMP . '/tmp.php')){
+        $message .= Message::errorMessage($language->getUserLanguage()["ERR_READ_ONLY_TMP_FILE"]);
+    }
 }else if(!isset($_POST['sql_host'], $_POST['sql_user'], $_POST['sql_bdd']) && !isset($_POST['site_name'], $_POST['site_color'], $_POST['site_email'], $_POST['site_pseudo'], $_POST['site_password'], $_POST['site_password_v'])){
     $message .= Message::warningMessage($language->getUserLanguage()["ERR_TMP_EXIST"]);
 
